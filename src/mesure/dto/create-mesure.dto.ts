95c070e0d9bf0648ab8df824f7c5c65848e8589b
Abdoul03 <samabdoul03@gmail.com>
2026-04-08 13:30:15 +0000

    mise en place de la verifcation dans mesur Dto

--- src/mesure/dto/create-mesure.dto.ts
+++ src/mesure/dto/create-mesure.dto.ts
@@ -15,31 +15,35 @@ export class CreateMesureDto {
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'La valeur de la taille ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur de la taille doit être positif' })
   taille: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Le poids ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur du poids doit être positif' })
   poids: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: "L'epaule ne doit pas de etre vide" })
   @IsPositive({ message: "La valeur de l'epaule doit être positif" })
   epaule: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'La valeur de la longueur du bras ne doit pas de etre vide',
+  })
   @IsPositive({ message: 'La valeur de la longeur du bras doit être positif' })
   longueurBras: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'La valeur de la longueur de la jambe ne doit pas de etre vide',
+  })
   @IsPositive({
     message: 'La valeur de la longeur de la jambe doit être positif',
   })
@@ -47,25 +51,25 @@ export class CreateMesureDto {
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'La valeur du cou ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur du cou doit être positif' })
   cou: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'La valeur du hanche ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur de la hanche doit être positif' })
   hanche: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'La valeur du poignet ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur du poignet doit être positif' })
   poignet: number;
 
   @ApiProperty()
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'La valeur du ventre ne doit pas de etre vide' })
   @IsPositive({ message: 'La valeur du ventre doit être positif' })
   ventre: number;
 }
